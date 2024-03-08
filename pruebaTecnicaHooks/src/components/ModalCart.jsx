49aa3d6e21affad807567cbfb21a6e63914c971d
DarioCastel <lecastelnovo@gmail.com>
2024-03-08 19:48:14 -0300

    cart v2

--- pruebaTecnicaHooks/src/components/ModalCart.jsx
+++ pruebaTecnicaHooks/src/components/ModalCart.jsx
@@ -1,34 +1,57 @@
-import {createPortal} from 'react-dom'
-import "../css/modalCart.css"
+import { createPortal } from "react-dom";
+import "../css/modalCart.css";
 import { IoCloseCircleOutline } from "react-icons/io5";
-import { useCart } from '../hooks/useCart';
-
+import { useCart } from "../hooks/useCart";
+import { useEffect, useState } from "react";
 
 const ModalCart = () => {
+  const { setOpenModal, itemToBuy } = useCart();
+  const handlerClose = () => {
+    setOpenModal(false);
+  };
 
+  const [acumulador, setAcumulador] = useState(0)
 
-  const {setOpenModal,itemToBuy}= useCart()
-  const handlerClose = ()=>{
-    setOpenModal(false)
-  }
-  console.log(itemToBuy)
-  return createPortal (
+  useEffect(() => {
+    itemToBuy.forEach((item) => {
+      setAcumulador(acumulador + item.subTotal)
+    });
+  
+  }, [itemToBuy])
+  
+  return createPortal(
     <>
-    <div className="modalContainer">
-    <IoCloseCircleOutline className="iconClose" onClick={handlerClose}/>
-      <h1>Carrito</h1>
-      {itemToBuy.length===0&&<span>{"carrito vacio"}</span>}
-      {itemToBuy.map((item)=>(
-        <div className="rendItem">
-          <span>{item.name}</span>
-          <span>{item.price}</span>
-          <span>{item.cant}</span>
-          <span>{item.subTotal}</span>
-        </div>
-      ))}
-    </div>
-    </>
-  ,document.getElementById("cart"))
-}
+      <div className="modalContainer">
+        <IoCloseCircleOutline className="iconClose" onClick={handlerClose} />
+        <h1>Carrito</h1>
+        {itemToBuy.length === 0 && <span>{"carrito vacio"}</span>}
+        {itemToBuy.length > 0 && (
+          <table className="cartTable">
+            <thead>
+              <th>{"Item name"}</th>
+              <th>{"Item price"}</th>
+              <th>{"Item cant"}</th>
+              <th>{"SubTotal"}</th>
+            </thead>
+            <tbody>
+              {itemToBuy.map((item) => (
+                <tr>
+                  <td>{item.title}</td>
+                  <td>{item.price}</td>
+                  <td>{item.cant}</td>
+                  <td className="tdSub">{item.subTotal}</td>
+                </tr>
+              ))}
+                <td className="nameTotal" colSpan="2" style={{textAlign:"left", borderTop:"1px solid black"}}>Total</td>
+                <td className="tdTotal" colSpan="2" style={{textAlign:"right", borderTop:"1px solid black"}}>{"$ "}{acumulador}</td>
 
-export default ModalCart
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>,
+    document.getElementById("cart")
+  );
+};
+
+export default ModalCart;
