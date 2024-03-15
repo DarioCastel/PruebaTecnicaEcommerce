c56b85b3ce31d6a6bd768dabf19ad1fba234332e
DarioCastel <lecastelnovo@gmail.com>
2024-03-15 20:10:06 -0300

    portalCartLocalStorageV1

--- pruebaTecnicaHooks/src/hooks/useCart.js
+++ pruebaTecnicaHooks/src/hooks/useCart.js
@@ -1,10 +1,19 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { CartContext } from "../context/CartContext"
 
 
 
 export function useCart (){
 const {openModal,setOpenModal,itemToBuy,setItemToBuy}= useContext(CartContext)
+
+
+useEffect(() => {
+  const cartLocal = localStorage.getItem("cart")
+  if(cartLocal){
+    setItemToBuy(JSON.parse(cartLocal))
+  }
+
+}, [])
 
 const addCart = (product) => {
     const existingItem = itemToBuy.find(item => item.title === product.title);
@@ -23,6 +32,7 @@ const addCart = (product) => {
       setItemToBuy(updatedItems);
     } else {
       setItemToBuy([...itemToBuy, product]);
+      localStorage.setItem("cart",JSON.stringify(itemToBuy))
     }
   };
 
@@ -30,8 +40,8 @@ const deleteItem= (titleToDelete)=>{
     const listFiltered= itemToBuy.filter(item => item.title !== titleToDelete)
     
     return(
-        setItemToBuy(listFiltered)
-        
+      setItemToBuy(listFiltered),
+      localStorage.setItem("cart",JSON.stringify(listFiltered))
     )
 }
 
