a47c27be68487188427cd2c25f8e77944f1f5a43
DarioCastel <lecastelnovo@gmail.com>
2024-03-15 19:22:20 -0300

    portalCartv2

--- pruebaTecnicaHooks/src/hooks/useCart.js
+++ pruebaTecnicaHooks/src/hooks/useCart.js
@@ -7,26 +7,21 @@ export function useCart (){
 const {openModal,setOpenModal,itemToBuy,setItemToBuy}= useContext(CartContext)
 
 const addCart = (product) => {
-    // Verificar si el producto ya está en el carrito
     const existingItem = itemToBuy.find(item => item.title === product.title);
   
     if (existingItem) {
-      // Si el producto ya existe, actualiza la cantidad y el subtotal
       const updatedItem = {
         ...existingItem,
         cant: existingItem.cant + product.cant,
         subTotal: existingItem.subTotal + product.subTotal
       };
   
-      // Crea un nuevo array con el elemento actualizado y los demás elementos sin cambios
       const updatedItems = itemToBuy.map(item =>
         item.title === product.title ? updatedItem : item
       );
   
-      // Actualiza el estado con el nuevo array modificado
       setItemToBuy(updatedItems);
     } else {
-      // Si el producto no existe, agrégalo al carrito
       setItemToBuy([...itemToBuy, product]);
     }
   };
